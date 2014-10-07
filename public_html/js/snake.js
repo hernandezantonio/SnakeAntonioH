3bcb25364307193249a012d023367d7c14be441d
antonio hernandez <defygaming99@gmail.com>
2014-10-07 10:40:22 -0700

    snake stuff

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -5,7 +5,14 @@ var screenwidth;
 var screenheight; 
 
 function gameInitialize(){
-    
+   var canvas = document.getElementById ("game-screen"); 
+   context = canvas.getContext ('2d'); 
+
+   screenwidth = window.innerwidth;
+   screenheight = window.innerHeight; 
+   
+   canvas.width = screenwidth; 
+
 }
 function gameLoop() {
     
