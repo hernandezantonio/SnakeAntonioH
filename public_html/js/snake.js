d44ec0ee8f3d22b5deb516220c9b26df86892715
antonio hernandez <defygaming99@gmail.com>
2014-10-24 11:27:42 -0700

    esnake

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -36,7 +36,7 @@ function gameInitialize(){
    screenWidth = window.innerWidth;
    screenHeight = window.innerHeight; 
    
-  canvas.width = screenWidth; 
+   canvas.width = screenWidth; 
    canvas.height = screenHeight; 
    
    document.addEventListener("keydown", keyboardHandler); 
@@ -111,17 +111,18 @@ function foodInitialize (){
 
  function foodDraw (){
      context.fillStyle = "white";
-     context.fillRect(food.x, food.y, snakeSize, snakeSize);
+     context.fillRect(food.x * snakeSize, food.y * snakeSize , snakeSize, snakeSize);
  }
 
 function setFoodPosition () {
     var randomX = Math.floor (Math.random() * screenWidth); 
     var randomY = Math.floor (Math.random() * screenHeight); 
-    food.x = randomX; 
-    food.y = randomY;
+    
+    food.x = Math.floor(randomX / snakeSize); 
+    food.y = Math.floor(randomY / snakeSize); 
 }
 
-/*$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+/*$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
  * Input Fuuntions
  * $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
  */
@@ -129,8 +130,12 @@ function setFoodPosition () {
 function keyboardHandler(event) {
     console.log(event); 
     
-    if(event.keyCode == "39") { 
+    if(event.keyCode == "39" && snakeDirection != "left") { 
         snakeDirection = "right"; 
         
     }
+    else if(event.keyCode == "40"&& snakeDirection != "up") {
+        snakeDirection = "down"; 
+    }
+    
 }
